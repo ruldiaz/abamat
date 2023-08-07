8f07cee5af165c06ef9fbc5c56dc4d22d231dfc6
RaulDiaz <rulhdiaz@gmail.com>
2023-08-07 10:13:01 -0600

    update

--- frontend/src/script.js
+++ frontend/src/script.js
@@ -1,4 +1,13 @@
 'use strict';
 
-const address = document.querySelector("#address");
+const address = document.querySelector('#address');
 console.log(address?.textContent);
+
+const paragraphs = document.querySelectorAll('p');
+console.log(paragraphs);
+
+paragraphs.forEach((paragraph) => {
+  console.log(paragraph.textContent);
+});
+
+console.log(paragraphs.length);
